ded2efa88a49c0ef18350c073dc10561ed1f1b11
Kishan Rajdev <kishan@Kishans-MacBook-Pro.local>
2016-05-02 00:58:00 +0530

    sh file

---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# MongoDB incremental backup
+
+#get increment no. between 1 to 10 based on date
+increment_no=$(date +"%d");
+increment_no=$((increment_no % 10));
+
+# creting directory for each backup
+dir_name="test_$increment_no";
+[ ! -d "$dir_name" ] && mkdir "$dir_name";
+
+#mongodump to export backup
+mongodump --db test --authenticationDatabase admin -u root -p 'root' --gzip -o ~/dump/$dir_name;
+
